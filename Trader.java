1c069634bd2369797c0c8560c9a802e2dc4cc235
deepthielindra <73148267+deepthielindra@users.noreply.github.com>
2020-10-31 02:53:53 +0530

    Add files via upload

---
+++ Trader.java
@@ -0,0 +1,24 @@
+package com.lab_assignment_8;
+
+public class Trader {
+	private final String name;
+	private final String city;
+	public String getName() {
+		return name;
+	}
+	public String getCity() {
+		return city;
+	}
+	
+	public Trader(String name, String city) {
+		super();
+		this.name = name;
+		this.city = city;
+	}
+	@Override
+	public String toString() {
+		return "Trader [name=" + name + ", city=" + city + "]";
+	}
+	
+	
+}
